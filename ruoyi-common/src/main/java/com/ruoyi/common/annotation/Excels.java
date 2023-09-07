ab328cf5f97dfd4e14630ae23847e84c5f32b0ec
wjh <1428823372@qq.com>
2023-09-07 15:13:01 +0800

    缺陷模块建立

--- ruoyi-common/src/main/java/com/ruoyi/common/annotation/Excels.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/annotation/Excels.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Excels
+public                                                                    @interface Excels
 {
     public Excel[] value();
 }
